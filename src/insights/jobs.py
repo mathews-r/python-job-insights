070969a798c68e6a7856b675b9f3844d403abaef
Mathews <mathewsrodrigues9@gmail.com>
2023-02-16 16:01:31 -0300

    adicionando funcao para buscar os tipos de jobs - requisito 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -15,21 +15,15 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
 
-    Must call `read`
+    jobs = read(path)
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    type_jobs = []
+    for job in jobs:
+        item = job["job_type"]
+        if item not in type_jobs:
+            type_jobs.append(job["job_type"])
+    return type_jobs
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
