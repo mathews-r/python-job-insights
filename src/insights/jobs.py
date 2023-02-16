04a72bbfe04cbe31acadf8d0274281d06f98d941
Mathews <mathewsrodrigues9@gmail.com>
2023-02-16 18:30:42 -0300

    criando funcao para filtrar job

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -27,18 +27,9 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    job_filtred = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            job_filtred.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    return job_filtred
