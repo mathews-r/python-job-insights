cb5ebacf661941a7b4f91609a24e4fa540e449e0
Mathews <mathewsrodrigues9@gmail.com>
2023-02-16 18:33:22 -0300

    criando funcao para filtrar industrias

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
 from src.insights.jobs import read
+
 # from jobs import read
 from typing import List, Dict
 
@@ -9,24 +10,15 @@ def get_unique_industries(path: str) -> List[str]:
     type_industries = []
     for industry in industries:
         item = industry["industry"]
-        if item not in type_industries and item != '':
+        if item not in type_industries and item != "":
             type_industries.append(industry["industry"])
     return type_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    industry_filtred = []
+    for item in jobs:
+        if item["industry"] == industry:
+            industry_filtred.append(item)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return industry_filtred
